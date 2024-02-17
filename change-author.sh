11c3ceaaca415abaeb57527c75a65a435bd67966
hima234 <himasri7862@gmail.com>
2024-02-17 21:44:47 +0530

    Updates

--- change-author.sh
+++ change-author.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
+new_email="himasri7862@gmail.com"
+new_name="hima234"
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 shanmukeshwar03@gmail.com himasri7862@gmail.com"
-    exit 1
-fi
-
-old_email=$1
-new_email=$2
-
-git filter-branch --env-filter "
-if [ \"\$GIT_COMMITTER_EMAIL\" = \"$old_email\" ]; then
-    export GIT_COMMITTER_NAME=\"hima234\"
+git filter-branch -f --env-filter "
+if [ \"$GIT_COMMITTER_EMAIL\" != \"$new_email\" ]; then
+    export GIT_COMMITTER_NAME=\"$new_name\"
     export GIT_COMMITTER_EMAIL=\"$new_email\"
 fi
-if [ \"\$GIT_AUTHOR_EMAIL\" = \"$old_email\" ]; then
-    export GIT_AUTHOR_NAME=\"hima234\"
+if [ \"$GIT_AUTHOR_EMAIL\" != \"$new_email\" ]; then
+    export GIT_AUTHOR_NAME=\"$new_name\"
     export GIT_AUTHOR_EMAIL=\"$new_email\"
-fi
-" --tag-name-filter cat -- --branches --tags
-
+fi" --tag-name-filter cat -- --branches --tags
